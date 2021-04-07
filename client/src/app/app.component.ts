ec9e3656afbe64da15dc9be7c6531425c385aa85
Prashant Bhatt <prashant.bhatt.ext@siemens.com>
2021-04-08 00:47:21 +0530

    changes to include login functionality partial update

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,11 @@ export class AppComponent  implements OnInit{
   users:any;
   ngOnInit():void{
     this.getUsers();
+    this.setCurrentUser();
+  }
+  setCurrentUser(){
+    const user:User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
   getUsers(){
     this.http.get<{id:string,name:string}>("https://localhost:5001/api/users").subscribe(response =>{
@@ -20,5 +27,5 @@ export class AppComponent  implements OnInit{
       console.log(error);
     });
   }
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private accountService:AccountService){}
 }
