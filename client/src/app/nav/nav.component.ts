ea74523ca1800e9ecf90a441c4ea0437edfcba78
Prashant Bhatt <prashant.bhatt.ext@siemens.com>
2021-04-13 00:03:18 +0530

    added toaster into the application

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +14,7 @@ export class NavComponent implements OnInit {
   model:any ={};
   //loggedIn:boolean;
   // currentUser$:Observable<User>;
-  constructor(public accountService:AccountService, private router:Router) { }
+  constructor(public accountService:AccountService, private router:Router, private toastr:ToastrService) { }
 
   ngOnInit(): void {
     // this.getCurrentUser();
@@ -26,6 +27,7 @@ export class NavComponent implements OnInit {
       this.router.navigateByUrl('/members');
     }, error =>{
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
