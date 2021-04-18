6e633a79e777bee2346451cb22c4b7cc3a412d3a
Prashant Bhatt <prashant.bhatt.ext@siemens.com>
2021-04-18 23:45:18 +0530

    End of section 10 - edit persisting changes, template forms, @viewchild

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 // const httpOptions = {
@@ -13,14 +15,28 @@ import { Member } from '../_models/member';
 export class MembersService {
 
   constructor(private http:HttpClient) { }
-  baseUrl = environment.apiUrl
+  baseUrl = environment.apiUrl;
+  members:Member[] =[];
   getMembers(){
-    return this.http.get<Member[]>(environment.apiUrl +'users');
+    if(this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(environment.apiUrl +'users').pipe(
+      map( members => {
+        this.members = members;
+        return members;
+      })
+    );
   }
   getMember(username:string){
+    const member = this.members.find(x => x.username === username);
+    if(member !== undefined) return of(member);
     return this.http.get<Member>(environment.apiUrl + 'users/'+username);
   }
   updateMember(member:Member){
-    return this.http.put(this.baseUrl+ 'users',member);
+    return this.http.put(this.baseUrl+ 'users',member).pipe(
+      map(() =>{
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
